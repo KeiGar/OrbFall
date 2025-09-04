b0380becc7c3d8900d817c7f4f7eecec24029ff5
kei <keigartner@gmail.com>
2025-09-04 22:52:17 +0900

    added brick scnee

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,13 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+const SPEED = 50000.0
 
 func _physics_process(delta: float) -> void:
 	var dir = 0
 	if Input.is_action_pressed("player_moveLeft"): dir = -1
 	if Input.is_action_pressed("player_moveRight"): dir = 1
 	
-	velocity.x = dir * SPEED
-	
+	velocity.x = dir * SPEED * delta
 
 	move_and_slide()
