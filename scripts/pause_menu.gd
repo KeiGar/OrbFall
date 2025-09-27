0e55f82d4b05b615af3d2c40f1bd21979eeb152b
kei <keigartner@gmail.com>
2025-09-27 15:29:15 +0900

    implemented continue and quit button

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,8 +1,17 @@
 extends BoxContainer
+const MAIN_MENU_CONTROLS = preload("uid://b3ceglkuwrcgy")
 
+signal btn_quit_pressed
+signal btn_continue_pressed
 
 func _on_game_pause() -> void:
 	visible = true
 
 func _on_game_unpause() -> void:
 	visible = false
+
+func _on_btn_continue_pressed() -> void:
+	btn_continue_pressed.emit()
+
+func _on_btn_quit_pressed() -> void:
+	btn_quit_pressed.emit()
