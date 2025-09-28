de9e939a63870073a49dac44cefda1989dd1d632
kei <keigartner@gmail.com>
2025-09-28 20:55:34 +0900

    sketch of highscore screen implemented

---
+++ scripts/menus/highscore_menu.gd
@@ -0,0 +1,4 @@
+extends BoxContainer
+
+func _on_btn_return_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menus/main_menu_controls.tscn")
--- scripts/menus/main_menu_controls.gd
+++ scripts/menus/main_menu_controls.gd
@@ -1,9 +1,12 @@
 extends BoxContainer
 const GAME = preload("uid://dld6rt8cylm2")
+const HIGHSCORE_MENU = preload("uid://dymimjcrkx72l")
 
 func _on_btn_start_pressed() -> void:
 	get_tree().change_scene_to_packed(GAME)
 
-
 func _on_btn_quit_game_pressed() -> void:
 	get_tree().quit(0)
+
+func _on_btn_highscores_pressed() -> void:
+	get_tree().change_scene_to_packed(HIGHSCORE_MENU)
