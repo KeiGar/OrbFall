8750686f307bd7c7578037ba6cb809ce8cfc2795
kei <keigartner@gmail.com>
2025-10-04 16:13:27 +0900

    ball change start

--- scripts/game_assets/ball.gd
+++ scripts/game_assets/ball.gd
@@ -1,9 +1,7 @@
-extends Area2D
+extends CharacterBody2D
 
 @export var SPEED = 600
 var direction
-@onready var boundary_left: ShapeCast2D = $"../BoundaryLeft"
-@onready var boundary_right: ShapeCast2D = $"../BoundaryRight"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,19 +12,17 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position = position + (direction * SPEED * delta)
-	if boundary_left.is_colliding():
-		direction = Vector2(1, direction.y).normalized()
-	if boundary_right.is_colliding():
-		direction = Vector2(-1, direction.y).normalized()
 
 func _on_body_entered(body: Node2D) -> void:
 	direction = Vector2(direction.x, -1).normalized()
 
 func _on_area_entered(area: Area2D) -> void:
-	var ydir = 0
-	var xDir = randf_range(-1,1)
-	if area.position.y >= position.y:
-		ydir = randf_range(-1,-.5)
-	else:
-		ydir = randf_range(.5,1)
-	direction = Vector2(xDir, ydir).normalized()
+	var dir = area.position - position
+	print(dir)
+	#var ydir = 0
+	#var xDir = randf_range(-1,1)
+	#if area.position.y >= position.y:
+		#ydir = randf_range(-1,-.5)
+	#else:
+		#ydir = randf_range(.5,1)
+	direction = -direction.normalized()
--- scripts/game_assets/killzone.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-signal killzone_hit
-
-func _on_area_entered(area: Area2D) -> void:
-	killzone_hit.emit()
