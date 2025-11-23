f53dbde094ebd3b1870cad5e87ee4cb8d6072a82
KeiGar <gartner.philipp@hotmail.com>
2025-11-23 15:27:16 +0900

    implemented pausing and unpausing

--- scripts/game.gd
+++ scripts/game.gd
@@ -4,27 +4,33 @@ extends Node2D
 @onready var game_over_label: Label = $GameOverScreen/GameOverLabel
 
 signal game_start
+signal pause
+signal unpause
+
+var isGamePaused = false
 
 func _ready() -> void:
 	game_over_label.visible = false
 	game_start.emit()
+	level_scenes.process_mode = Node.PROCESS_MODE_DISABLED
 	
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("quit_game"):
 		get_tree().quit()
-
-
-func _on_game_timer_pause() -> void:
-	level_scenes.process_mode = Node.PROCESS_MODE_DISABLED
-
-
-func _on_game_timer_unpause() -> void:
-	level_scenes.process_mode = Node.PROCESS_MODE_ALWAYS
-	game_timer.queue_free()
-
+	if Input.is_action_just_pressed("pause_game"):
+		if isGamePaused:
+			isGamePaused = false
+			level_scenes.process_mode = Node.PROCESS_MODE_ALWAYS
+			unpause.emit()
+		else:
+			isGamePaused = true
+			level_scenes.process_mode = Node.PROCESS_MODE_DISABLED
+			pause.emit()
 
 func _on_killzone_game_over() -> void:
 	level_scenes.process_mode = Node.PROCESS_MODE_DISABLED
 	game_over_label.visible = true
-	
-	
+
+func _on_game_start_timer_timeout() -> void:
+	level_scenes.process_mode = Node.PROCESS_MODE_ALWAYS
+	game_timer.queue_free()
--- scripts/game_timer.gd
+++ scripts/game_timer.gd
@@ -4,30 +4,15 @@ extends Node2D
 
 const GAME_START_COUNTDOWN := 4
 
-signal pause
-signal unpause
-
-
 func _process(delta: float) -> void:
 	if game_start_timer.time_left > .8:
 		timer_display.text = "%.0f" % game_start_timer.time_left
 	else:
 		timer_display.text = "START!" 
 
-
-func game_pause() -> void:
-	game_start_timer.start(GAME_START_COUNTDOWN)
-	print("Game paused!")
-	pause.emit()
-
-func game_unpause() -> void:
-	print("Game unpaused!")
-	unpause.emit()
-
-
 func _on_game_start_timer_timeout() -> void:
-	game_unpause()
-
+	print("Game unpaused!")
 
 func _on_game_game_start() -> void:
-	game_pause() 
+	game_start_timer.start(GAME_START_COUNTDOWN)
+	print("Game start!")
