257dccb3b211ea9e0fb345661ae62f939a5b6656
KeiGar <gartner.philipp@hotmail.com>
2026-03-07 12:57:45 +0900

    implemented mute button

--- scripts/menus/mute_button.gd
+++ scripts/menus/mute_button.gd
@@ -1,18 +1,33 @@
 extends Control
-@onready var btn_mute: TextureButton = $BTN_Mute
-@onready var btn_unmute: TextureButton = $BTN_Unmute
+@onready var btn: TextureButton = $BTN
+const SPEAKER_MUTE = preload("uid://dgkc5ulm7burk")
+const SPEAKER_UNMUTE = preload("uid://bqqdaokqnqe1c")
 
-var is_muted := false
+var is_muted: bool
+const AUDIO_BUS_MAIN := 0
+
+func _ready() -> void:
+	is_muted = GlobalSettings.sound_muted
+	load_texture()
+
+func load_texture() -> void:
+	if is_muted:
+		btn.texture_normal = SPEAKER_MUTE
+	else:
+		btn.texture_normal = SPEAKER_UNMUTE
+		
 
 func toggle_mute() -> void:
-	if !is_muted:
-		btn_unmute.visible = true
-		btn_mute.visible = false
-		is_muted = true
-	else:
-		btn_unmute.visible = false
-		btn_mute.visible = true
-		is_muted = false
+	is_muted = !is_muted
+	AudioServer.set_bus_mute(AUDIO_BUS_MAIN, is_muted)
+	GlobalSettings.sound_muted = is_muted
 	
 func _on_btn_pressed() -> void:
 	toggle_mute()
+	load_texture()
+
+func _on_mouse_entered() -> void:
+	btn.scale = Vector2(1.25, 1.25)
+
+func _on_mouse_exited() -> void:
+	btn.scale = Vector2(1, 1)
