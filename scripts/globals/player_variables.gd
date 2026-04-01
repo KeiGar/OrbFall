c98602fc478582cc17f6c16309f5312c7e315c1e
KeiGar <gartner.philipp@hotmail.com>
2026-04-01 23:06:04 +0900

    implemented highscore api manager that connects to aws and invokes lambda

--- scripts/globals/player_variables.gd
+++ scripts/globals/player_variables.gd
@@ -1,3 +1,4 @@
 extends Node
 var player_score:int = 0
 var player_name:String = ""
+var player_uid := OS.get_unique_id()
---
+++ scripts/utils/highscore_manager_api.gd
@@ -0,0 +1,67 @@
+extends Node
+
+const BASE_URL = "https://api.keigartner.com/orbfall/highscore"
+
+var Highscores_Received: Array[HighscoreManager.HighscoreData] = []
+
+signal leaderboard_received(data)
+signal score_upload_success
+
+func _ready() -> void:
+	$HTTPRequest.request_completed.connect(_on_request_completed)
+	fetch_highscore() # fetch highscore on game boot
+
+func upload_highscore(player_name: String, score: int) -> void:
+	print("Uploading score for ", player_name)
+	var data = { 
+		"player_id": PlayerVariables.player_uid,
+		"player_name": player_name,
+		"score": score
+	}
+	var body = JSON.stringify(data)
+	var headers = ["Content-Type: application/json"]
+	var response = $HTTPRequest.request(BASE_URL, headers, HTTPClient.METHOD_POST, body)
+	if response != OK:
+		push_error("An error ocurred while attempting to save the Highscore to the database.")
+		
+func fetch_highscore() -> void:
+	print("Fetching top scores...")
+	var headers = ["Content-Type: application/json"]
+	var error = $HTTPRequest.request(BASE_URL, headers, HTTPClient.METHOD_GET)
+	if error != OK:
+		push_error("An error occurred while attempting to fetch the score from the database.")
+		
+# --- Unified Response Handler ---
+func _on_request_completed(result: int, response_code: int, _headers: PackedStringArray, body: PackedByteArray):
+	if response_code != 200:
+		print("API Error! Code: ", response_code)
+		print("Response: ", body.get_string_from_utf8())
+		return
+
+	var json_data = JSON.parse_string(body.get_string_from_utf8())
+	
+	if json_data == null:
+		print("Failed to parse JSON response.")
+		return
+
+	# Logic to distinguish between the POST and GET responses
+	if json_data.has("leaderboard"):
+		# This is a GET response
+		emit_signal("leaderboard_received", json_data["leaderboard"])
+		_print_scores(json_data["leaderboard"])
+		
+	elif json_data.has("message"):
+		# This is a POST response (from our Lambda 'save_highscore' return)
+		print("Server says: ", json_data["message"])
+		emit_signal("score_upload_success")
+
+func _print_scores(scores: Array):
+	print("--- CURRENT LEADERBOARD ---")
+	for i in range(scores.size()):
+		var entry = scores[i]
+		print("%d. %s: %d" % [i + 1, entry["name"], entry["score"]])
+		
+func _on_leaderboard_received(data) -> void:
+	Highscores_Received.clear()
+	for entry in data:
+		Highscores_Received.append(HighscoreManager.HighscoreData.new(entry.name, entry.score))
