35b555cef6cb995820d0e416c8e79193ee0c26e8
KeiGar <gartner.philipp@hotmail.com>
2026-04-24 01:16:45 +0900

    fixed uuid generation for web app

--- scripts/globals/player_variables.gd
+++ scripts/globals/player_variables.gd
@@ -1,4 +1,7 @@
 extends Node
 var player_score:int = 0
 var player_name:String = ""
-var player_uid := OS.get_unique_id()
+var player_uid := GameUtils.generate_uuid()
+
+func _ready() -> void:
+	print(player_uid)
