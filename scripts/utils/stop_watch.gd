8bac6142d84f03e4b22bcf0d9b30fefb27e9e6fb
KeiGar <gartner.philipp@hotmail.com>
2026-06-29 21:59:39 +0900

    Refactor stopwatch to use elapsed time for display and simplify time conversion

--- scripts/utils/stop_watch.gd
+++ scripts/utils/stop_watch.gd
@@ -1,18 +1,14 @@
 extends BoxContainer
 @onready var lbl_time: Label = $LBLTime
 
-var time_start:= Time.get_ticks_msec()
-var time_curr:= Time.get_ticks_msec() - time_start
 var time_elapsed:= 0.0
 
 func _process(delta: float) -> void:
-	time_curr = Time.get_ticks_msec() - time_start
 	time_elapsed += delta
-	lbl_time.text = str(time_elapsed).pad_decimals(2)
-	# lbl_time.text = _converTimeToStringFormatFromMsec(time_curr)
+	lbl_time.text = convert_time_to_string_format_from_sec(time_elapsed)
 
-func _converTimeToStringFormatFromMsec(timeCurr: int) -> String: 
-	var seconds:= (int)(timeCurr / 1000.0)
+func convert_time_to_string_format_from_sec(timeCurr: float) -> String:
+	var seconds:= (int)(timeCurr)
 	var minutes:= (int)(seconds / 60.0)
 	var secondsRemainder:= (int)(seconds % 60)
 	var timeDisplay: String = "%02d:%02d" % [minutes, secondsRemainder]
